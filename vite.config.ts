b7fa5caf81fdfd3bc87bdcc3dce99eb370645a28
Ziba Nouri <zibanouri13621115@gmail.com>
2025-11-02 21:34:17 +0330

    update App.tsx and vite.cofig

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/nova/',
-  plugins: [react()],
-});
+  base:'/nova/',
+  plugins: [
+    react(),
+     tailwindcss()
+    ],
+})
