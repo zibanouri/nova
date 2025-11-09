16c31fcb5fe6caf3bfa50e1650878a340c1e9f45
Ziba Nouri <zibanouri13621115@gmail.com>
2025-11-09 23:32:07 +0330

    add dashboard component in code

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,8 +1,16 @@
 const Dashboard = () => {
-    return(
-        <div>dashboard</div>
-    )
-}
+    return (
+        <div className="min-h-screen">
+            <div id="DashboardHeader"></div>
+
+            <div className="flex">
+                <div id="DashboardSidebar"></div>
+                  <main className="flex-1 transition-all duration-300 ml-64 pt-16 min-h-screen flex flex-col overflow-x-hidden">
+          <div id="DashboardContent" className="flex-1"></div>
+        </main>
+            </div>
+        </div>
+    )}
 
 
 
