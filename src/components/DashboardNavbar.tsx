7bcb92457e7fc4c1a5026846cbce7a47741d0329
Ziba Nouri <zibanouri13621115@gmail.com>
2025-11-19 14:01:53 +0330

    add new icon for dashboard navbar

---
+++ src/components/DashboardNavbar.tsx
@@ -0,0 +1,7 @@
+import { Moon, Sun, User } from 'lucide-react'; 
+// import { useState, useEffect } from 'react';
+
+const DashboardNavbar = () => {
+  
+
+export default DashboardNavbar;
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,4 @@
-import Dashboard from '../components/Dashboard.tsx';
+import Dashboard from '../components/DashboardNavbar.tsx';
 const Index = () => {
   return <Dashboard />;
 };
