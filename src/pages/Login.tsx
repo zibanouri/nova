4140260eec18ef0dd8e452549ddc244132fdfe6c
Ziba Nouri <zibanouri13621115@gmail.com>
2025-11-21 00:23:14 +0330

    add card ui for login component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 const Login = () => {
     return (
         <div>Login</div>
