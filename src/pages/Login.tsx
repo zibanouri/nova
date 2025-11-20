5f7223ae61cd31674c3f4566713571812ebeb01c
Ziba Nouri <zibanouri13621115@gmail.com>
2025-11-21 00:24:25 +0330

    add eye icon lucide react

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Eye, EyeOff } from "lucide-react";
 const Login = () => {
     return (
         <div>Login</div>
