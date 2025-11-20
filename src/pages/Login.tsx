1b0ba37c82c35e7139b972872bc4d5ee55921db3
Ziba Nouri <zibanouri13621115@gmail.com>
2025-11-21 00:25:36 +0330

    add use state in login component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,6 +3,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
 const Login = () => {
     return (
         <div>Login</div>
