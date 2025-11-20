c1e247933e855e9fdb199b294f96ccfd8415e1cf
Ziba Nouri <zibanouri13621115@gmail.com>
2025-11-20 20:55:59 +0330

    add dropdown ui

--- src/components/DashboardNavbar.tsx
+++ src/components/DashboardNavbar.tsx
@@ -1,6 +1,12 @@
 import { Moon, Sun, User } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 const DashboardNavbar = () => {
     return (
         <div>navbar</div>
