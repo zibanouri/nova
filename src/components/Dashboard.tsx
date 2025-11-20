ce6a89d1fb0e606b2f70b73eb027d5b47261408d
Ziba Nouri <zibanouri13621115@gmail.com>
2025-11-21 02:06:49 +0330

    update styles dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
     <div className="min-h-screen">
       <DashboardNavbar />
       <main className="pt-16 p-4">
-        {/* <h1 className="text-2xl font-bold">Dashboard</h1> */}
+      <h1 className="text-2xl font-bold">Dashboard</h1>
         {/* <p className="mt-2 text-muted-foreground">
           You are logged in 
         </p> */}
