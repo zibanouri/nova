d4ad27f2dfb47b67403466e74dabe7ff1bfaeccb
Ziba Nouri <zibanouri13621115@gmail.com>
2025-11-21 00:22:18 +0330

    add label ui for login component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 const Login = () => {
     return (
         <div>Login</div>
