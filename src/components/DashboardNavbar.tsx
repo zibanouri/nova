a79f9e60430f2e0c4a730e338c78bc0110ea3a89
Ziba Nouri <zibanouri13621115@gmail.com>
2025-11-20 20:53:39 +0330

    add avatar ui

--- src/components/DashboardNavbar.tsx
+++ src/components/DashboardNavbar.tsx
@@ -1,5 +1,6 @@
 import { Moon, Sun, User } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 const DashboardNavbar = () => {
     return (
         <div>navbar</div>
