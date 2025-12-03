cc0d0b9de40f3713c6922672a5b29b82e5cbbb17
Ziba Nouri <zibanouri13621115@gmail.com>
2025-12-03 14:36:56 +0330

    add login page in app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Index from './pages/Index';
-import Login from './pages/Login';
+import LoginPage from './pages/LoginPage';
 function App() {
   return (
     <BrowserRouter basename="/nova">
       <Routes>
-        <Route path="/login" element={<Login />} />
+         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<Index />} />
         <Route path="*" element={<Index />} />
       </Routes>
