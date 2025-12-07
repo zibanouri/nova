e23f67bd75265108190b0a38a9890b2a2e1a6cc6
Ziba Nouri <zibanouri13621115@gmail.com>
2025-12-07 10:05:27 +0330

    add input and label ui in code

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -7,6 +7,8 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
 const RegisterPage = () =>{
     return (
