3a2efd5083b11832ec1e4ea542e9246ba51de1bf
Ziba Nouri <zibanouri13621115@gmail.com>
2025-12-07 09:54:57 +0330

    add not found page in code

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Index from './pages/Index';
 import LoginPage from './pages/LoginPage';
+import NotFound from './pages/NotFound';
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
