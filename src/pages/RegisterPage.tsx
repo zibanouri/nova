c311457dc10fe148dc375cefc2c76ccb306fa454
Ziba Nouri <zibanouri13621115@gmail.com>
2025-12-07 10:04:24 +0330

    add button and card ui in code

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,3 +1,13 @@
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
 const RegisterPage = () =>{
     return (
         <div>RegisterPage</div>
